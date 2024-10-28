3e0046c8c79ec596719c44c189daae26015399ea
Unknown <113107552+DenisIk1@users.noreply.github.com>
2024-10-28 22:15:37 +0400

    Add files via upload

---
+++ Scp3114CustomSpawn/EventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Extensions;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using PlayerRoles;
+
+namespace Scp3114CustomSpawn
+{
+    public class EventHandler
+    {
+        public void OnSpawning(SpawningEventArgs ev)
+        {
+            if(ev.Player.Role.Type == PlayerRoles.RoleTypeId.Scp3114 && (Round.ElapsedTime.TotalSeconds < 2 || MainPlugin.Instance.Config.Debug))
+            {
+                if(Server.PlayerCount < MainPlugin.Instance.Config.MinPlayers)
+                {
+                    Timing.CallDelayed(0.01f, () =>
+                    {
+                        ev.Player.Role.Set(MainPlugin.Instance.Config.RolesToSpawn.GetRandomValue());
+                    });
+                }
+                else
+                {
+                    ev.Position = RoleTypeId.Scp173.GetRandomSpawnLocation().Position;
+                }
+            }
+        }
+    }
+}
